14bae13546da7bce1912a188422e8fb9f51bf50f
LevanTedo <94954894+LevanTedo@users.noreply.github.com>
2025-04-21 17:39:22 +0200

    init

---
+++ H14.py
@@ -0,0 +1,11 @@
+L = [1,2,6,4,5,4,3,2,4,7,6]
+subList1 = [2,6,4]
+subList2 = [1,2,3]
+subList3 = [5]
+
+def check_sublist(list1,list2):
+    if len(list1)>len(list2):
+        for x in range(list1):
+            if list1[x] == list2[0]:
+    return None
+    
