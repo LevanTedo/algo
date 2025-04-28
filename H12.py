7cdbf57e1b7412a79725b8840bbccc817b4663df
LevanTedo <94954894+LevanTedo@users.noreply.github.com>
2025-04-28 12:32:05 +0200

    lol

--- H12.py
+++ H12.py
@@ -1,7 +1,7 @@
 L = [1,1,1,2,2,3,4,5,6,6,7,8,9,10]
 
 def removeDuplicates(L):
-    """Diese Funktion nimmt eine sortierte Liste L mit mehrfach vorkommenden Zahlen 
+    """ Diese Funktion nimmt eine sortierte Liste L mit mehrfach vorkommenden Zahlen 
         und erstellt eine Ergebnisliste result. Der erste Wert von L wird direkt übernommen
         und bei den folgenden Werten wird geprüft, ob der Wert dem aktuellsten Wert in der result-Liste
         gleicht. Sollte dies nicht der Fall sein, wird der Wert aus L der result-Liste hinzugefügt."""
---
+++ H24.py
@@ -0,0 +1,21 @@
+list = [1,2,3]
+
+def swap(l,i,j):
+    dummy = l[i]
+    l[i] = l[j]
+    l[j] = dummy
+
+def rotate_right(l):
+    for x in range(len(l)):
+        if x is not len(l):
+            
+        else:
+            swap(l)
+
+def rotatelist(L,s):
+    
+
+
+
+
+
